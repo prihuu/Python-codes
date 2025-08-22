a9ec976e4d45b85cabecb5fb0600550f009c76b7
awahn <awahnde.princess@gmail.com>
2025-08-22 15:37:17 +0300

    percentage

--- hello.py
+++ hello.py
@@ -1,1 +1,35 @@
+"""
 print("Hello, World!")
+print("Hello")
+
+print("World")
+
+user = input("Enter your name:")
+print("Nice to see you, "+user+"!")
+print(type(user))
+a = 10
+b = 2.5
+c = 6+3j
+d = "Python"
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(30>20)
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(a/b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+"""
+
+a = int(input("Enter your math scores: "))
+b = int(input("Enter your physics scores: "))
+c = int(input("Enter your programming scores: "))
+d = int(a+b+c/90*100)
+
+print("Your total score is", d , "%")
