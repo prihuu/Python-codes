cc7b6f2eee81adb5c08463845ff510fa70f14753
awahn <awahnde.princess@gmail.com>
2025-09-06 23:54:41 +0300

    excercise

--- hello.py
+++ hello.py
@@ -99,7 +99,7 @@ elif gender == "male":
         print("hemoglobin level is high!")
 else:
     print("Invalid gender!")
-"""
+
 
 year = int(input("Enter a year:"))
 if year % 4 == 0:
@@ -112,3 +112,78 @@ if year % 4 == 0:
         print("The year is a leap year.")
 else:
     print("The year is not a leap year.")
+
+num = 1
+while num <= 1000:
+    if num % 3 == 0:
+        print(num)
+    num += 1
+
+while True:
+    inches = float(input("Enter length in inches: "))
+    if inches < 0:
+        print("Program ended.")
+        break
+    centimeters = inches * 2.54
+    print(f"{inches} inches = {centimeters:.2f} cm")
+
+
+smallest = None
+largest = None
+
+while True:
+    user_input = input("Enter a number: ")
+    if user_input == "":
+        break
+    number = float(user_input)
+    if smallest is None or number < smallest:
+        smallest = number
+    if largest is None or number > largest:
+        largest = number
+if smallest is None:
+    print("No numbers were entered.")
+else:
+    print("Smallest number:", smallest)
+    print("Largest number:", largest)
+
+
+import random
+secret = random.randint(1, 10)
+while True:
+   number = int(input("Guess a number between 1 and 10: "))
+   if number < secret:
+    print("Too low!")
+   elif number > secret:
+    print("Too high!")
+   else:
+    print("Correct!")
+
+
+
+correct_username = "python"
+correct_password = "rules"
+max_attempts = 5
+attempts = 0
+while attempts < max_attempts:
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    if username == correct_username and password == correct_password:
+        print("Welcome!")
+        break
+    else:
+        print("Incorrect username or password.")
+        attempts += 1
+if attempts == max_attempts:
+    print("Access denied.")
+"""
+
+import random
+N = int(input("Enter the number of random points to generate: "))
+circle = 0
+for i in range(N):
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+    if x ** 2 + y ** 2 < 1:
+        circle += 1
+pi_approx = 4 * circle / N
+print("Approximation of pi:", pi_approx)
